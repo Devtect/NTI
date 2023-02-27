e711fe7cd8073d5fd6a9d8bdcd1ed91488795e71
devtect <kaihanucoz@gmail.com>
2023-02-27 18:34:21 +0100

    updated inheritance

--- Programming 2/3. inheritance/inheritance.py
+++ Programming 2/3. inheritance/inheritance.py
@@ -1,15 +1,20 @@
 # Inheritance allows to use the properties and methods of one class in another
 
 # general class human (parent class)
+
 class human:
-    def __init__(self, brain):
-        self.brain = brain
-    
-    
+    def __init__(self, speak):
+        self.speak = speak
 
-class person(human):
-    def __init__(self, brain):
-        super().__init__(brain)
-
-    def name(self, name):
-        print(f"My name is {self.name}!")
+# general class wolf (parent class)   
+class wolf:
+    def __init__(self, strength):
+        self.strength = strength
+       
+# sub class werewolf (child class)
+class werewolf(human, wolf):
+    def __init__(self, speak, strength):
+        super().__init__(speak, strength)
+       
+    def __init__(self, fur):
+        self.fur = fur
