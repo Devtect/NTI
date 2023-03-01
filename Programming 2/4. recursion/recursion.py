e6dccb5ffe34cbee377712b7a23da615c88e8d81
devtect <kaihanucoz@gmail.com>
2023-03-01 19:34:57 +0100

    added recursion

---
+++ Programming 2/4. recursion/recursion.py
@@ -0,0 +1,22 @@
+'''
+recursion is a function that calls itself from within
+recursion helps to visualize a complex problem into steps,
+which can be solved more easily(iteratively  and recursively)
+'''
+
+
+# ITERATIVE (fast - complex)
+# def walk (steps):
+#     for step in range(1, steps + 1):
+#         print(f"You take step {step}")
+
+
+# RECURSIVE (slower - simpler)
+def walk (steps):
+    if steps == 0:  # base condition/when do we stop
+        return 
+    walk(steps - 1)
+    print(f"You take step {steps}")
+
+
+walk(100)
