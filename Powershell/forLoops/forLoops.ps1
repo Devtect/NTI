7c172b5ae12c0a2d5a538ae42eed53d58b365c7b
Devtect <kaihanucoz@gmail.com>
2023-03-06 16:00:48 +0100

    added pwshl files

--- Powershell/forLoops/forLoops.ps1
+++ Powershell/forLoops/forLoops.ps1
@@ -4,7 +4,7 @@ for (variable = value; condition for counting; increase/decease value) {
     command to repeat 
 }
 #>
-$MyArray = @("benji", "oliver", "kaihan", "piotr")
+$MyArray = @("bmw", "lexus", "ferrai", "nissan")
 for ($i = 0; $i -lt $MyArray.Count; $i ++){
     Write-Output ("Element: $i Value:" + $MyArray[$i])
 }
