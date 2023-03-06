211b390f93c5ed0321fe2071608cc6c373983999
Devtect <kaihanucoz@gmail.com>
2023-03-06 08:46:45 +0100

    fixed forLoop

--- TillämpadProgrammering/Powershell/forLoops/forLoops.ps1
+++ TillämpadProgrammering/Powershell/forLoops/forLoops.ps1
@@ -6,6 +6,6 @@ for (variable = value; condition for counting; increase/decease value) {
 #>
 
 $MyArray = @("Reaper", "Warrior", "Paladin")
-for ($i = 0; $i -lt $MyArray.Count ++; $i ++) {
+for ($i = 0; $i -lt $MyArray.Count; $i ++) {
     echo ("Element $i value: " + $MyArray[$i])
 }
