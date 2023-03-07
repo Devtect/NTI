7e2992dc922546bd0203d75dc625ab88464ec7d0
Devtect <kaihanucoz@gmail.com>
2023-03-07 20:33:08 +0100

    raiden

---
+++ Programming 2/1. dataclasses/dataclasses.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+
+@dataclass
+
+class fighter:
+    name: str
+    attack: int
+    defense: int
+    level: int
+    boost: int
+
+    def __str__(self):
+        return f"Name: {self.name} -- Level: {self.level}"
