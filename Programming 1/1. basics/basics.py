0233b380650efe5215ba85d92c8350af7ec7bf92
devtect <kaihanucoz@gmail.com>
2023-03-08 23:37:12 +0100

    basics

--- Programming 1/1. basics/basics.py
+++ Programming 1/1. basics/basics.py
@@ -13,9 +13,12 @@ int_ = 5
 bool_ = 2.4
 boolean_ = True / False
 # Printing to terminal
-
+print("Hello, my name is no name")
 # Taking user input
-
+question = input("What is your name?")
+print(question)
 # Format (f-string)
-
+print(f"My name is {question}")
 # Operators
++ - / *
+** // % 
