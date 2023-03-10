7d2dbe89e1c5359670e22800342a0cce7c49d049
Devtect <kaihanucoz@gmail.com>
2023-03-10 14:36:32 +0100

    while loops upt

--- Powershell/whileLoops/whileLoop.ps1
+++ Powershell/whileLoops/whileLoop.ps1
@@ -1,7 +1,10 @@
-while ($userinput -ne "quit"){
-    $userinput = Read-Host "Type 'yes' if you love powershell(type quit to exit)"
+<#
+while(condition){
+    command you want to run
 
-    if($userinput -eq "yes") {Write-Output "You are getting an A!"}
-    elseif ($userinput -eq "no") {Write-Output "You are getting an F!"}
+    additional logical operators if neeeded
 }
-$userinput = $null
+variable reset
+#>
+
+
